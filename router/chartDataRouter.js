a6c4a2df3cd3ac2419e4e0356cf28f42dfa18ca6
jihunhong <redgee49@gmail.com>
2019-08-26 15:30:52 +0900

    Refactor router functions

--- router/chartDataRouter.js
+++ router/chartDataRouter.js
@@ -2,37 +2,24 @@ const genie = require('../chart/genie.json');
 const melon = require('../chart/melon.json');
 const bugs = require('../chart/bugs.json');
 
-module.exports = function(app)
-{
-    app.get('/genie', function(req, res){
-        res.json(genie);
+const charts = {
+    'genie': genie,
+    'melon': melon,
+    'bugs': bugs
+};
+
+module.exports = function (app) {
+    app.get('/:chart', function (req, res) {
+        const name = req.params.chart;
+        res.json(charts[name]);
     })
 
-    app.get('/genie/:rank', function(req, res){
+    app.get('/:chart/:rank', async function (req, res) {
+        const name = req.params.chart;
+        const chart = charts[name];
+
         const rank = req.params.rank;
-        const music = genie.find((v) => v.rank == rank);
-
-        res.json(music);
-    })
-
-    app.get('/melon', function(req, res){
-        res.json(melon);
-    })
-
-    app.get('/melon/:rank', function(req, res){
-        const rank = req.params.rank;
-        const music = melon.find((v) => v.rank == rank);
-
-        res.json(music);
-    })
-
-    app.get('/bugs', function(req, res){
-        res.json(bugs);
-    })
-
-    app.get('/bugs/:rank', function(req, res){
-        const rank = req.params.rank;
-        const music = bugs.find((v) => v.rank == rank);
+        const music = await chart.find((v) => v.rank == rank);
 
         res.json(music);
     })
