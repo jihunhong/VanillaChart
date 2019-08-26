50d60b122d5e8b18f71e58897588a1758dd8a185
jihunhong <redgee49@gmail.com>
2019-08-26 15:22:05 +0900

    Find element by rank

--- router/chartDataRouter.js
+++ router/chartDataRouter.js
@@ -8,12 +8,33 @@ module.exports = function(app)
         res.json(genie);
     })
 
+    app.get('/genie/:rank', function(req, res){
+        const rank = req.params.rank;
+        const music = genie.find((v) => v.rank == rank);
+
+        res.json(music);
+    })
+
     app.get('/melon', function(req, res){
         res.json(melon);
     })
 
+    app.get('/melon/:rank', function(req, res){
+        const rank = req.params.rank;
+        const music = melon.find((v) => v.rank == rank);
+
+        res.json(music);
+    })
+
     app.get('/bugs', function(req, res){
         res.json(bugs);
+    })
+
+    app.get('/bugs/:rank', function(req, res){
+        const rank = req.params.rank;
+        const music = bugs.find((v) => v.rank == rank);
+
+        res.json(music);
     })
 
 }
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const router = require('./router/chartDataRouter.js/index.js')(app);
+const router = require('./router/chartDataRouter.js')(app);
+const bodyParser = require('body-parser');
 
 app.use(express.json());
 app.use(express.static('public'));
+app.use(bodyParser.json());
+
 
 const server = app.listen(3000, function(){
     console.log("Express server has started on port 3000");
