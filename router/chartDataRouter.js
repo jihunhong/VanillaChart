588b525828eb96f779eb7c3d5a846e43ab4967ff
jihunhong <redgee49@gmail.com>
2019-08-29 19:40:37 +0900

    Refactor the api that was processed as a variable

--- router/chartDataRouter.js
+++ router/chartDataRouter.js
@@ -13,20 +13,43 @@ const charts = {
 };
 
 module.exports = function (app) {
-    app.get('/api/:chart/list', function (req, res) {
-        const name = req.params.chart;
-
-        res.json(result.success(charts[name]));
+    app.get('/api/genie', function (req, res) {
+        res.json(result.success(charts[genie]));
     })
 
-    app.get('/api/:chart', function (req, res) {
-        const name = req.params.chart;
-        const chart = charts[name];
+    app.get('/api/genie/:rank', function (req, res) {
+        const genie = charts['genie'];
 
-        const keys = Object.keys(req.query);
-        const queryKey = keys[0];
-        const music = chart.find((v) => v[queryKey] == req.query[queryKey]);
+        const music = genie.find((v) => v['rank'] == req.params['rank']);
 
         res.json(result.judge(music));
     })
+
+
+    app.get('/api/melon', function (req, res) {
+        res.json(result.success(charts[melon]));
+    })
+
+    app.get('/api/melon/:rank', function (req, res) {
+        const melon = charts['melon'];
+
+        const music = melon.find((v) => v['rank'] == req.params['rank']);
+
+        res.json(result.judge(music));
+    })
+
+
+    app.get('/api/bugs', function (req, res) {
+        res.json(result.success(charts[bugs]));
+    })
+
+    app.get('/api/bugs/:rank', function (req, res) {
+        const bugs = charts['bugs'];
+
+        const music = bugs.find((v) => v['rank'] == req.params['rank']);
+
+        res.json(result.judge(music));
+    })
+
+    
 }
