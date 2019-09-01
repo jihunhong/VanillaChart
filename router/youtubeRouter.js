d8243fee6207d80dff39d9e786d50c8016750d88
jihunhong <redgee49@gmail.com>
2019-09-01 19:09:50 +0900

    Add videoId retrieval via query api

--- router/youtubeRouter.js
+++ router/youtubeRouter.js
@@ -1,7 +1,19 @@
+const JSONResult = require('../Object/JSONResult.js');
+const VIDEOIDS = require('../data.json');
+
+const result = new JSONResult();
+
 module.exports = function(app)
 {
-    app.get('/youtube', function(req, res){
-        
+    app.get('/api/youtube/search', function(req, res){
+        if(Object.keys(req.query).length === 0){
+            res.json(result.fail('need to input query'));
+        }else{
+            const query = req.query.q;
+            const video = VIDEOIDS.find((v) => v.query == query);
+            res.json(result.judge(video));
+        }
+
     })
 
 }
