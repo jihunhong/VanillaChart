ef2a45f1a9018c4c21fc2d9641873b036cb49088
jihunhong <redgee49@gmail.com>
2019-09-05 20:46:30 +0900

    Add react (client folder)

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 
-app.listen(3000, () => 
-    console.log("Express server has started on port 3000")
+app.listen(5000, () => 
+    console.log("Express server has started on port 5000")
 );
 
