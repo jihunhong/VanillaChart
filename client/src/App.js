86df3616e6be2e840457830018a9a9cd825fdd16
jihunhong <redgee49@gmail.com>
2019-09-07 03:49:04 +0900

    Add chart component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
-import Chart from './components/charts/chart';
+import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Navbar from './components/Nav/navbar';
+import Chart from './components/Chart/chart';
+import Topchart from './components/TopChart/topchart';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Navbar />
+      <Topchart />
       <Chart />
     </div>
   );
