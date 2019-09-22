109dee47f73f90e0ab6747d122499b090ad824b4
jihunhong <redgee49@gmail.com>
2019-09-23 04:29:42 +0900

    Add 'production' mode settings

--- server.js
+++ server.js
@@ -9,14 +9,16 @@ const bodyParser = require('body-parser');
 app.use(express.json());
 app.use(bodyParser.json());
 
+if( process.env.NODE_ENV == 'production'){
 
-app.use(express.static(path.join(__dirname, 'client', 'bulid' )));
+    console.log('Production MODE resolved');
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
+    app.use(express.static(path.join(__dirname, './client', 'bulid')));
 
-
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 8080;
 
