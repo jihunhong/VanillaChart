eb4c8d195e5d688a97900243a7ae616434638bb9
jihunhong <redgee49@gmail.com>
2019-09-22 14:13:54 +0900

    Add production setting

--- server.js
+++ server.js
@@ -7,22 +7,20 @@ const youtubeRouter = require('./router/youtubeRouter.js')(app);
 const bodyParser = require('body-parser');
 
 app.use(express.json());
-app.use(express.static('public'));
 app.use(bodyParser.json());
 
 
-if( process.env.NODE_ENV === 'production' ){
-    app.use(express.static( 'client/bulid' ));
+app.use(express.static(path.join(__dirname, 'client', 'bulid' )));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => 
-    console.log("Express server has started on port 5000")
+    console.log("Express server has started on port 8080")
 );
 
