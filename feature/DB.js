cf02679338301adff0ec97f5853b93f1f78697b6
jihunhong <redgee49@gmail.com>
2019-10-14 02:42:44 +0900

    Add insert test code

--- feature/DB.js
+++ feature/DB.js
@@ -13,6 +13,24 @@ MongoClient.connect(url, function(err, client) {
   console.log("Connected successfully to server");
  
   const db = client.db(dbName);
+  
+  insertDocuments(db, function() {
+    client.close();
+  });
  
-  client.close();
 });
+
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
