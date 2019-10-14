ef19574ccc681e8db699f88743cae8f329f83015
jihunhong <redgee49@gmail.com>
2019-10-14 22:06:20 +0900

    Add test code to insert chart data

--- feature/youtube.js
+++ feature/youtube.js
@@ -11,8 +11,16 @@ const bugs  = require('../chart/bugs.json');
 
 const youtubeAPI = require('../logs/youtubeAPI.json');
 
-
 const limit = 3;
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'myproject';
 
 function searching(music, name) {
     
@@ -42,6 +50,7 @@ function searching(music, name) {
 
             fs.writeFileSync( path.join('../logs/youtubeAPI.json')  , JSON.stringify(youtubeAPI, null, 2));
             fs.writeFileSync( path.join('../apiKeys.json')  , JSON.stringify(apiKeys, null, 2));
+            
             let responseItems = [];
 
             try{
@@ -83,3 +92,30 @@ async function insertVideoId(){
 }
 
 insertVideoId();
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+   
+    const db = client.db(dbName);
+    
+    insertDocuments(db, function() {
+      client.close();
+    });
+   
+  });
+  
+const insertDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Insert some documents
+    collection.insert( 
+        genie , function(err, result) {
+      assert.equal(err, null);
+      
+      assert.equal(50 , result.ops.length);
+      console.log("Inserted 3 documents into the collection");
+      callback(result);
+    });
+}
