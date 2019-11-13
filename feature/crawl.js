60b4df2367a24aee0137069d25ae5cae804cd68d
jihunhong <redgee49@gmail.com>
2019-11-13 19:28:28 +0900

    Refactor setter method

--- feature/crawl.js
+++ feature/crawl.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const Chart = require('../Object/Chart.js');
 
-const melon = new Chart('melon', 'https://www.melon.com/chart/', 'tr.lst50' ,'div.rank01 a', '.ellipsis.rank02 > span', '.image_typeAll');
+const melon = new Chart();
 
 melon.setName('melon');
 melon.setUrl('https://www.melon.com/chart/');
@@ -13,11 +13,24 @@ melon.setTitle('div.rank01 a');
 melon.setArtist('.ellipsis.rank02 > span');
 melon.setImg('.image_typeAll');
 
-const genie = new Chart('genie', 'https://www.genie.co.kr/chart/top200','td.info', 'a.title', 'a.artist', 'a.cover');
-const bugs  = new Chart('bugs' ,  'https://music.bugs.co.kr/chart', 'tbody tr' ,'p.title a', 'p.artist a', 'a.thumbnail');
+const genie = new Chart();
+
+genie.setName('genie');
+genie.setUrl('https://www.genie.co.kr/chart/top200');
+genie.setParent('td.info');
+genie.setTitle('a.title');
+genie.setArtist('a.artist');
+genie.setImg('a.cover');
+
+const bugs  = new Chart();
+
+bugs.setName('bugs');
+bugs.setUrl('https://music.bugs.co.kr/chart')
+bugs.setParent('tbody tr')
+bugs.setTitle('p.title a')
+bugs.setArtist('p.artist a')
+bugs.setImg('a.thumbnail')
 
 melon.getData();
 genie.getData();
 bugs.getData();
-
-// module.exports = melon.getData();
