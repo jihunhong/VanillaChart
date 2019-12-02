b45798530fcfdd0db47b995909ab72ca257c9ce3
jihunhong <redgee49@gmail.com>
2019-12-03 00:57:14 +0900

    Add patch method api on chart api

--- router/chartApi.js
+++ router/chartApi.js
@@ -10,6 +10,7 @@ router.get('/:chart', async(req, res) => {
 
     try{
         const collection = await chart.find();
+        // 차트 이름 별로 모든 데이터 조회
         res.json(collection);
     }catch(err){
         res.json({message : err});
@@ -18,6 +19,19 @@ router.get('/:chart', async(req, res) => {
 
 router.get('/:chart/:rank',  async(req, res) => {
     
+    const chart = mongoose.model('Chart', chartSchema, req.params.chart)
+
+    try{
+        const music = await chart.find({rank: req.params.rank});
+        res.json(music);
+    }catch(err){
+        res.json({message : err});
+    }
+
+})
+
+router.patch('/:chart/:id',  async(req, res) => {
+    // 조회한 음원 항목의 데이터 수정
     const chart = mongoose.model('Chart', chartSchema, req.params.chart)
 
     try{
