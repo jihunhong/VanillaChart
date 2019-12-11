74d3dd762c5cb6f3c09f53b4dd300f96d91f5a79
jihunhong <redgee49@gmail.com>
2019-12-11 15:22:03 +0900

    Refactor models

--- models/Chart.js
+++ models/Chart.js
@@ -1,26 +1,11 @@
 const mongoose = require('mongoose');
 
 const chartSchema = mongoose.Schema({
-    rank: {
-        type: Number,
-        require: true
-    },
-    title : {
-        type: String,
-        require: true
-    },
-    artist: {
-        type: String,
-        require: true
-    },
-    img : {
-        type: String,
-        require: true
-    },
-    video_id: {
-        type: String,
-        require: false
-    }
+    rank:     {type: Number, require: true },
+    title :   {type: String, require: true },
+    artist:   {type: String, require: true },
+    img :     {type: String, require: true },
+    video_id: {type: String, require: false}
 });
 
 module.exports = chartSchema;
