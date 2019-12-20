575722cd2b9c2b8c3ed59ef929b92f8d3828218e
jihunhong <redgee49@gmail.com>
2019-12-21 01:01:42 +0900

    Config passport-setup

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,19 +1,22 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
-
-const keys = require('./keys');
+const YoutubeV3Strategy = require('passport-youtube-v3').Strategy;
+const fs = require('fs');
+const keys = require('../keys');
 
 passport.use(
-    new GoogleStrategy({
-        // options for the google strategy
+    new YoutubeV3Strategy({
+
         callbackURL: '/auth/google/redirect',
         clientID: keys.google.clientID,
-        clientSecret: keys.google.clientSecret
+        clientSecret: keys.google.clientSecret,
+        scope: ['https://www.googleapis.com/auth/youtube'],
+        profileURL: 'https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true'
 
     }, (accessToken, refreshToken, profile, done) => {
-        // passport callback function
-        console.log('passport callback function fired');
 
+        console.log('passport callback function fired');
         console.log(profile);
+        fs.writeFileSync('mine.json', JSON.stringify(profile));
+
     })
 )
