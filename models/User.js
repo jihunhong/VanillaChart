4a89af5827e1c47d28a082df77e7b43d1ed3addc
jihunhong <redgee49@gmail.com>
2019-12-31 22:24:46 +0900

    Add fetch api in personal-routes.js

--- models/User.js
+++ models/User.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: String,
     googleId: String,
+    accessToken: String,
+    refreshToken: String
 })
 
 const User = mongoose.model('user', userSchema);
--- routes/personal-routes.js
+++ routes/personal-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const fetch = require('node-fetch');
 
 const authCheck = (req, res, next) => {
     if(!req.user){
@@ -10,7 +11,21 @@ const authCheck = (req, res, next) => {
 }
 
 router.get('/', authCheck, (req, res) => {
-    res.send('Hi ! ' + req.user.username);
+    res.json(req.user);
 })
+
+router.get('/playlist', (req, res) => {
+    fetch('https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true', {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer ' + req.user.accessToken,
+        }
+    }).then(response => response.json())
+        .then(data => {
+            res.send(data);
+        })
+})
+
 
 module.exports = router;
