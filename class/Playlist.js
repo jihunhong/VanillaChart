c4d059c7a12349cfe78d42d746a89ef498c11e56
jihunhong <redgee49@gmail.com>
2020-01-22 23:57:40 +0900

    Add playlist import feature (melon)

--- class/Playlist.js
+++ class/Playlist.js
@@ -1,39 +1,50 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 
 const mongoose = require('mongoose');
 
 class Playlist{
     constructor(url, id){
         this.id = id;
-        this.url = 'https://www.melon.com/mymusic/playlist/mymusicplaylistview_inform.htm?plylstSeq=' + this.id;
+        this.url = url;
     }
 
     Builder(obj){
         this.setId(obj.id);
+        this.setUrl(obj.url);
     }
 
     setId(id){
         this.id = id;
     }
 
+    setUrl(url){
+        this.url = url;
+    }
+
     async getList(){
-        const getHTML = async () => {
-            try{
-                return await axios.get(this.url);
-            }catch(e){
-                console.error(e);
-            }
-        }
+        const browser = await puppeteer.launch({headless : true});
 
-        const HTML = await getHTML();
-        const $ = cheerio.load(HTML.data);
+        const page = await browser.newPage();
 
-        const tr = $('tr');
+        await page.goto(this.url);
+        // 사용자 플레이리스트 url
 
-        console.log($(tr[1]).find('span.odd_span'));
 
+        const titles = await page.evaluate(() => {
+            const array = Array.from(document.querySelectorAll('td.t_left > div.wrap > .ellipsis > a.fc_gray'));
+            return array.map(title => title.textContent);
+        })
+
+        const artists = await page.evaluate(() => {
+            const array = Array.from(document.querySelectorAll('td.t_left > div.wrap > #artistName > a.fc_mgray'));
+            return array.map(artist => artist.textContent);
+        })
         
+        await browser.close();
+
+        const chart = titles.map((v, i) => { return {title : v, artist: artists[i]} });
+
+        return chart;
     }
 }
 
