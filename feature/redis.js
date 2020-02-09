ee5d5f6c8c2c94e0014a7eab9f757d07cac8ff52
jihunhong <redgee49@gmail.com>
2020-02-09 20:12:06 +0900

    Add setting chart data code using redis

---
+++ feature/redis.js
@@ -0,0 +1,28 @@
+const redis = require('redis');
+const client = redis.createClient();
+
+const mongoose = require('mongoose');
+const db = require('../keys.js').db;
+
+const chartSchema = require('../models/Chart');
+
+(async() => {
+
+    await mongoose.connect(
+        db.uri, 
+        { useNewUrlParser: true,
+          useUnifiedTopology: true},
+        () => console.log('connected')
+    )
+
+    
+
+    const melon = await mongoose.model('Chart', chartSchema, 'melon').find();
+    const genie = await mongoose.model('Chart', chartSchema, 'genie').find();
+    const bugs = await mongoose.model('Chart', chartSchema, 'bugs').find();
+
+    await client.set('melon', JSON.stringify(melon), redis.print);
+    await client.set('genie', JSON.stringify(genie), redis.print);
+    await client.set('bugs', JSON.stringify(bugs), redis.print);
+   
+})();
