6badad9710be4c76a9b65f3425be202625d290eb
jihunhong <redgee49@gmail.com>
2020-02-10 00:08:12 +0900

    Correct chart rest api params

--- routes/chart-routes.js
+++ routes/chart-routes.js
@@ -27,12 +27,12 @@ router.get('/:chart/:rank',  async(req, res) => {
     
     try{
         
-        client.get(req.params.chart, (err, value) => {
+        client.get(req.params.chart, async (err, value) => {
             if( err ){console.log(err)};
 
             const json = JSON.parse(value);
 
-            const data = json.find((v) => v.rank === req.params.rank );
+            const data = json.find((v) => v.rank == req.params.rank );
 
             res.json(data);
         });
@@ -42,7 +42,7 @@ router.get('/:chart/:rank',  async(req, res) => {
     }
 })
 
-router.get('/:chart/:time', async(req, res) => {
+router.get('/:chart/time/:time', async(req, res) => {
     // 시간을 받아서 과거 차트 데이터 조회
     const pastCharts = mongoose.model('pastCharts', pastChartsSchema, 'pastcharts');
 
