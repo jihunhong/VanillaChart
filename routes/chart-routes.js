da394df622ffeb6901e2dec42bc6944d59dc65d2
jihunhong <redgee49@gmail.com>
2020-12-25 22:28:30 +0900

    chart 검색 API

--- routes/chart-routes.js
+++ routes/chart-routes.js
@@ -6,52 +6,35 @@ const { Chart } = require('../models');
 const redis = require('redis');
 const client = redis.createClient();
 
-router.get('/:chart', async(req, res) => {
+router.get('/:chart', async(req, res, next) => {
 
     try{
-
-        client.get(req.params.chart, (err, value) => {
-            if( err ){console.log(err)};
-            
-            res.json(JSON.parse(value));
-        });
-
+        const chart = await Chart.findOne({
+            where : {
+                site : req.params.chart,
+            }
+        })
+        res.status(200).send(chart);
     }catch(err){
-        res.json({message : err});
+        console.error(err);
+        next(err);
     }
 })
 
 router.get('/:chart/:rank',  async(req, res) => {
     
     try{
-        
-        client.get(req.params.chart, async (err, value) => {
-            if( err ){console.log(err)};
-
-            const json = JSON.parse(value);
-
-            const data = json.find((v) => v.rank == req.params.rank );
-
-            res.json(data);
-        });
-
+        const result = await Chart.findOne({
+            where : {
+                site : req.params.chart,
+                rank : req.params.rank
+            }
+        })
+        res.status(200).send(result);
     }catch(err){
-        res.json({message : err});
+        console.error(err);
+        next(err);
     }
-})
-
-router.get('/:chart/time/:time', async(req, res) => {
-    // 시간을 받아서 과거 차트 데이터 조회
-    const pastCharts = mongoose.model('pastCharts', pastChartsSchema, 'pastcharts');
-
-    try{
-        // const data = await pastCharts.find({data: req.params.rank});
-
-        res.json(data);
-    }catch(err){
-        res.json({message : err});
-    }
-
 })
 
 module.exports = router;
