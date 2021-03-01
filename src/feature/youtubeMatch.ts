f0e6b8d7e28c2525ebf81e91ca59129f301453ea
jihunhong <redgee49@gmail.com>
2021-03-02 00:51:21 +0900

    youtube API 검색 타이핑, 함수 작성

--- src/feature/youtubeMatch.ts
+++ src/feature/youtubeMatch.ts
@@ -1,24 +1,43 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+import path from 'path';
+import { matchedItem } from '../../@types/search';
+
+dotenv.config({ path : path.join(__dirname, '../../.env') });
 const KEY = process.env.YOUTUBE_API_KEY;
 const API_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 async function search({ q }: { q : string }) {
     try{
         const res = await axios.get(`${API_URL}?part=snippet&q=${q}&key=${KEY}`);
-        const { data } = res;
-        console.log(data);
+        return res.data;
     }catch(error){
         console.error(error);;
         throw error;
     }
 }
 
+function arrange(items: any){
+    return items.map((v: any) => {
+        return {
+            title : v.snippet.title,
+            videoId : v.id.videoId,
+            description : v.snippet.description,
+            publishedAt : v.snippet.publishedAt,
+            thumbnail : v.snippet.thumbnails.high || v.snippet.thumbnails.medium || v.snippet.thumbnails.default,
+            channelTitle : v.snippet.channelTitle,
+            channelId : v.snippet.channelId,
+        }
+    })
+}
+
 
 (async() => {
     try{
-        const q = 'Celebrity';
-        const result = await search({ q });
-        console.log(result);
+        const q = 'Celebrity - 아이유';
+        const { items } = await search({ q });
+        const matchedList: Array<matchedItem> = arrange(items);
+        console.log(matchedList);
     }catch(err){
         console.log(err);
     }
