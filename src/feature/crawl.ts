bc828b857e0da3200c002be8db2795ab47887285
jihunhong <redgee49@gmail.com>
2021-04-20 13:49:37 +0900

    crawl 개발단 코드 수정

--- src/feature/crawl.ts
+++ src/feature/crawl.ts
@@ -7,16 +7,16 @@ import { createYoutubeRows } from './youtubeMatch';
 (async() => {
     const { browser, page } = await launchBrowser();
     try{
-        // const genie = await collectGenie({ page });
-        // await insertChart({ site : 'genie', chart : genie });
+        const genie = await collectGenie({ page });
+        await insertChart({ site : 'genie', chart : genie });
         
-        // const melon = await collectMelon({ page });
-        // const convertedMelon = await ftsMatchingJob({ chart : melon });
-        // await insertChart({ site : 'melon', chart : convertedMelon });
+        const melon = await collectMelon({ page });
+        const convertedMelon = await ftsMatchingJob({ chart : melon });
+        await insertChart({ site : 'melon', chart : convertedMelon });
 
-        // const bugs = await collectBugs({ page });
-        // const convertedBugs = await ftsMatchingJob({ chart : bugs });
-        // await insertChart({ site : 'bugs', chart : convertedBugs });
+        const bugs = await collectBugs({ page });
+        const convertedBugs = await ftsMatchingJob({ chart : bugs });
+        await insertChart({ site : 'bugs', chart : convertedBugs });
 
         await createYoutubeRows();
         
